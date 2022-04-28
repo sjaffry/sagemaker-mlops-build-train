cbc19b5f5e5336c4f4fd16f6d467d795aa39a2e1
Syed <syejaffr@amazon.com>
2022-04-28 21:53:47 +0000

    demo commit 5

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -177,7 +177,7 @@ def get_pipeline(
     xgb_train.set_hyperparameters(
         objective="reg:linear",
         num_round=50,
-        max_depth=5,
+        max_depth=10,
         eta=0.2,
         gamma=4,
         min_child_weight=6,
